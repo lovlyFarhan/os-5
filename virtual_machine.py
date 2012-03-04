f46473c1cf25ab40e1ec39111d5adf3b976e38bb
lunacik <lunacik1@yahoo.pl>
2012-03-04 20:19:07 +0200

    filled ds and cs

--- virtual_machine.py
+++ virtual_machine.py
@@ -8,8 +8,24 @@ class VirtualMachine():
         self.SS = "A0"
         self.IP = self.CS
         self.SP = self.SS
-        fill_mem(proc)
+        self.fill_mem(proc)
 
 
     def fill_mem(self, proc):
-        pass   
+        DS = proc.commands[1:proc.commands.index("CODE")]
+        CS = proc.commands[proc.commands.index("CODE") + 1:]
+        DS_counter = int(self.DS, 16)
+        for cmd in DS:
+            if(cmd[0:2] == "DW"):
+                self.memory["%X" %DS_counter] = int(cmd[3:])
+            else:
+                self.memory["%X" %DS_counter] = cmd[3:]
+            DS_counter += 1
+
+        CS_counter = int(self.CS, 16)
+        for cmd in CS:
+            self.memory["%X" %CS_counter] = cmd
+            CS_counter += 1
+
+        print(self.memory)
+            
